af4857434186de19aa248c8f5a538c2472eda0fd
Ekaterina Kaiuda <kayudaev@gmail.com>
2024-05-28 20:46:45 -0300

    removed extra tests

--- mail_search.py
+++
@@ -1,6 +0,0 @@
-from selene import browser, be, have
-
-
-browser.open('https://ya.ru')
-browser.element('[name="text"]').should(be.blank).type('121blablabla').press_enter()
-browser.element('[id="search-result"]').should(have.no.text('121pupupuuu'))
