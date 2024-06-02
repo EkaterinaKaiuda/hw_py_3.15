c898eb51cd8ba3bdb5c532b5a5bee6efa45e992d
Ekaterina Kaiuda <kayudaev@gmail.com>
2024-06-02 14:05:59 -0300

    Added first test and fixed second

--- monkey_test.py
+++ monkey_test.py
@@ -1,10 +1,15 @@
 from selene import browser, be, have
 
 
+def test_positive():
+    browser.open('/')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
+
 def test_find_element():
     browser.open('/')
     non_existent_value = 'erfvrbbtytmntybmbtvmvyfn'
 
     browser.element('[name="q"]').should(be.blank).type(non_existent_value).press_enter()
     browser.element('.card-section [role=heading]').should(
-        have.text(f'{non_existent_value}'))
+        have.text(f'No se han encontrado resultados para tu búsqueda ({non_existent_value}).'))
